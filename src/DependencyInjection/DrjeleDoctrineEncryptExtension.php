d780c3086011305d7873d21084e4ab7d4e93063c
Constantin Jeledintan <constantin.jeledintan@multinode.ro>
2020-10-12 17:51:02 +0300

    fix services load

--- src/DependencyInjection/DrjeleDoctrineEncryptExtension.php
+++ src/DependencyInjection/DrjeleDoctrineEncryptExtension.php
@@ -22,7 +22,7 @@ class DrjeleDoctrineEncryptExtension extends Extension
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
-        $loader->load('encryptors.yaml');
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
