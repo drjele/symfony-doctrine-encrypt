7920b4b48b052f473f08157ee291763a4f58e4e3
Constantin Jeledintan <constantin.jeledintan@multinode.ro>
2020-10-12 18:15:25 +0300

    encrypt/decrypt db commands

---
+++ src/Command/AbstractCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) Adrian Jeledintan
+ */
+
+namespace Drjele\DoctrineEncrypt\Command;
+
+use DateTime;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+abstract class AbstractCommand extends Command
+{
+    /** @todo add memory and time limits */
+    protected InputInterface $input;
+    protected OutputInterface $output;
+    protected SymfonyStyle $io;
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->io->title(sprintf('<bg=blue>[%s]</> %s', (new DateTime())->format('Y-m-d'), $this->getName()));
+    }
+
+    protected function writeln(string $text): void
+    {
+        $this->io->writeln(
+            sprintf(
+                '<bg=blue>[%s][%s]</> %s',
+                (new DateTime())->format('H:i:s'),
+                $this->getMemoryUsage(),
+                $text
+            )
+        );
+    }
+
+    protected function error(string $text): void
+    {
+        $this->io->error(
+            sprintf(
+                '[%s][%s] %s',
+                (new DateTime())->format('H:i:s'),
+                $this->getMemoryUsage(),
+                $text
+            )
+        );
+    }
+
+    private function getMemoryUsage(): string
+    {
+        $size = memory_get_usage();
+        $unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+
+        $memory = round($size / pow(1024, ($i = floor(log($size, 1024)))), 2);
+
+        return number_format($memory, 2, '.', '') . ' ' . $unit[$i];
+    }
+}
---
+++ src/Command/DecryptDatabaseCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) Adrian Jeledintan
+ */
+
+namespace Drjele\DoctrineEncrypt\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+class DecryptDatabaseCommand extends AbstractCommand
+{
+    protected static $defaultName = 'drjele:doctrine:database:decrypt';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+        } catch (Throwable $e) {
+            $this->error($e->getMessage());
+
+            return static::FAILURE;
+        }
+
+        return static::SUCCESS;
+    }
+}
