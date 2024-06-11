bced13a578fa6e2b4d13380b4408d26eba4b7880
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2024-06-11 10:00:35 +0300

    v4.3.1 services.yaml -> services.php

--- src/DependencyInjection/DrjeleDoctrineEncryptExtension.php
+++ src/DependencyInjection/DrjeleDoctrineEncryptExtension.php
@@ -11,17 +11,14 @@ namespace Drjele\Doctrine\Encrypt\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 class DrjeleDoctrineEncryptExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
-        $loader->load('services.yaml');
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.php');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
---
+++ src/Resources/config/services.php
@@ -0,0 +1,57 @@
+<?php
+
+use Drjele\Doctrine\Encrypt\Command\AbstractDatabaseCommand;
+use Drjele\Doctrine\Encrypt\Command\DatabaseDecryptCommand;
+use Drjele\Doctrine\Encrypt\Command\DatabaseEncryptCommand;
+use Drjele\Doctrine\Encrypt\Encryptor\AbstractEncryptor;
+use Drjele\Doctrine\Encrypt\Encryptor\AES256Encryptor;
+use Drjele\Doctrine\Encrypt\Encryptor\AES256FixedEncryptor;
+use Drjele\Doctrine\Encrypt\Encryptor\FakeEncryptor;
+use Drjele\Doctrine\Encrypt\Service\EncryptorFactory;
+use Drjele\Doctrine\Encrypt\Service\EntityService;
+use Symfony\Component\DependencyInjection\Argument\TaggedIteratorArgument;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
+
+return function (ContainerConfigurator $configurator) {
+    $services = $configurator->services();
+
+    $encryptorTag = 'drjele.doctrine.encryptor';
+
+    $services->set(AbstractEncryptor::class)
+        ->abstract()
+        ->arg('$salt', '%drjele_doctrine_encrypt.salt%');
+
+    $services->set(AES256Encryptor::class)
+        ->parent(AbstractEncryptor::class)
+        ->tag($encryptorTag);
+
+    $services->set(FakeEncryptor::class)
+        ->tag($encryptorTag);
+
+    $services->set(AES256FixedEncryptor::class)
+        ->parent(AbstractEncryptor::class)
+        ->tag($encryptorTag);
+
+    $services->set(EncryptorFactory::class)
+        ->public()
+        ->arg('$encryptors', new TaggedIteratorArgument($encryptorTag));
+
+    $services->set(AbstractDatabaseCommand::class)
+        ->abstract()
+        ->arg('$managerRegistry', new Reference('doctrine'))
+        ->arg('$encryptorFactory', new Reference(EncryptorFactory::class))
+        ->arg('$entityService', new Reference(EntityService::class));
+
+    $services->set(DatabaseEncryptCommand::class)
+        ->parent(AbstractDatabaseCommand::class)
+        ->tag('console.command');
+
+    $services->set(DatabaseDecryptCommand::class)
+        ->parent(AbstractDatabaseCommand::class)
+        ->tag('console.command');
+
+    $services->set(EntityService::class)
+        ->arg('$managerRegistry', new Reference('doctrine'))
+        ->arg('$encryptorFactory', new Reference(EncryptorFactory::class));
+};
